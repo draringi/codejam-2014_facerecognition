329e6994cea261d4964f2ff9251c67301784b951
Michael Williams <draringi@draringi.net>
2014-11-15 17:26:23 -0500

    Let's try a different Algo...

--- face-trainer/headers/trainer.h
+++ face-trainer/headers/trainer.h
@@ -8,7 +8,7 @@
 #ifndef TRAINER_H_
 #define TRAINER_H_
 
-#define RECOGNITION_ALGO_BUILDER createFisherFaceRecognizer()
+#define RECOGNITION_ALGO_BUILDER createLBPHFaceRecognizer()
 
 #include <queue>
 #include <string>
